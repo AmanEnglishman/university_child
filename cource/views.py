3edf0a246fe38b49aa39dc41866251b519e0581a
Aman <aman13032006@gmail.com>
2025-03-22 16:58:48 +0600

    second

--- cource/views.py
+++ cource/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Classes
+
+
+class ClassesListView(generic.ListView):
+    model = Classes
+    template_name = 'index.html'
+    context_object_name = 'classes'
