7695a118c5e724ba15a8f1990b61d5524879ea1f
Aman <aman13032006@gmail.com>
2025-03-22 17:54:09 +0600

    second

--- cource/urls.py
+++ cource/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import ClassesListView
 
 urlpatterns = [
-    path('', ClassesListView.as_view(), name='classes_list'),
+    path('', ClassesListView.as_view(), name='index'),
 ]
